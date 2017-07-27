eff1165cb613ffe91c38c7a9dd2b80bb711a1b32
Jerry <one_way@aliyun.com>
2017-07-27 13:57:39 +0800

    Initial commit

---
+++ src/main/java/com/imooc/jms/producer/ProducerServiceImpl.java
@@ -0,0 +1,31 @@
+package com.imooc.jms.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import javax.jms.Session;
+
+public class ProducerServiceImpl implements ProducerService {
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Resource(name = "queueDestination")
+    Destination destination;
+    public void sendMessage(final String message) {
+        //使用JmsTemplate发送消息
+        jmsTemplate.send(destination, new MessageCreator() {
+            //创建一个消息
+            public Message createMessage(Session session) throws JMSException {
+                TextMessage textMessage=session.createTextMessage(message);
+                return session.createTextMessage(message);
+            }
+        });
+        System.out.println("发送消息："+message);
+    }
+}
